d14673225399091b2dab82b122605d8c70dd85a9
Shreyash Shinkar <62178952+shreyash04@users.noreply.github.com>
2022-05-16 18:51:04 +0530

    A3 - Selection Sort

---
+++ 31373_A3_selection_sort.py
@@ -0,0 +1,23 @@
+A = []
+n = int(input("Enter length of array: "))
+for i in range (0,n):
+    print("Enter element ",i+1,"below")
+    element = int(input())
+    A.append(element)
+
+print("You entered the array: ", A)
+# Traverse through all array elements
+for i in range(len(A)):
+	
+	min_idx = i
+	for j in range(i+1, len(A)):
+		if A[min_idx] > A[j]:
+			min_idx = j
+			
+	# Swap the found minimum element with the first element		
+	A[i], A[min_idx] = A[min_idx], A[i]
+
+# Driver code to test above
+print ("Sorted array")
+for i in range(len(A)):
+	print("%d" %A[i],end=" ")
